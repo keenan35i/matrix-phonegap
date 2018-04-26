81eac145cf6484ae59fcb5337465020bd980f000
lauram <mti.laura01@gmail.com>
2018-04-26 13:25:31 +0300

    switched to staging

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2,9 +2,9 @@ var appVersion = '1.16';
 
 var store = window.localStorage;
 //var domain = 'www.edu-20.com';
-var domain = 'www.matrixlms.com';
+//var domain = 'www.matrixlms.com';
 //var domain = 'www.neolms.com';
-//var domain = 'www.matrix-lms.com';
+var domain = 'www.matrix-lms.com';
 
 //branded apps
 //var domain = 'mckesson.medpower.org';
